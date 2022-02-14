e2b13a5ef813de341806348e71a191b41fc64fe4
TomazRojc <tr8906@student.uni-lj.si>
2022-02-14 21:39:11 +0100

    Version update

---
+++ The_Dungeon/Assets/Scripts/PickUpTorch.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpTorch : MonoBehaviour
+{
+    public Transform destination;
+
+    public void PickUp() {
+        this.transform.position = destination.position;
+        this.transform.parent = GameObject.Find("TestPlayer").transform;
+    }
+
+    public void Drop() {
+        this.transform.parent = null;
+    }
+}
