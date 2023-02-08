384a6ca614f8b130cedd0124c5ff2df4af64a912
TomazRojc <tr8906@student.uni-lj.si>
2023-02-08 13:29:40 +0100

    Fix UI scales, add escape menu

---
+++ Assets/Scripts/EscapeMenuUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EscapeMenuUI : MonoBehaviour {
+    
+    [SerializeField] private GameObject escapeMenuCanvas;
+    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (escapeMenuCanvas.activeSelf) {
+                CloseEscapeMenu();
+            } else {
+                OpenEscapeMenu();
+            }
+        }
+    }
+    
+    public void CloseEscapeMenu() {
+        escapeMenuCanvas.SetActive(false);
+    }
+    
+    private void OpenEscapeMenu() {
+        escapeMenuCanvas.SetActive(true);
+    }
+    
+}
