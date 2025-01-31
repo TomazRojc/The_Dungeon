1c3f6f87f3825a43579e2f922aef8e825c4fc389
jan-rojc <jan.rojc2000@gmail.com>
2025-01-31 18:23:25 +0100

    added new input system WIP

---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.ReadValue<Vector2>());
+    }
+}
