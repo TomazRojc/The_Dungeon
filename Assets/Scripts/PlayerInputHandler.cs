da547a615fa6464fedfcc00987d428539ab02033
jan-rojc <jan.rojc2000@gmail.com>
2025-02-01 23:39:10 +0100

    added player input prefab for input handling

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,10 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    private PlayerInput _playerInput;
+    private int _playerIndex;
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _playerIndex = _playerInput.playerIndex;
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<Vector2>());
+        Debug.Log("Move: " + context.ReadValue<Vector2>());
+    }
+    
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Debug.Log("Look: " + context);
+    }
+    
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire: " +  context);
     }
 }
