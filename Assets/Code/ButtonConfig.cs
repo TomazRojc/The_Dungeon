4d1fae9446620a0c4fadde70a275cfe554a3b521
TomazRojc <tomaz.rojc2000@gmail.com>
2025-02-15 11:03:13 +0100

    Add button config, background image, OnClick event

---
+++ Assets/Code/ButtonConfig.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code
+{
+    [CreateAssetMenu(fileName = "ButtonConfig", menuName = "ScriptableObjects/ButtonConfig", order = 1)]
+    public class ButtonConfig : ScriptableObject
+    {
+        [SerializeField]
+        private Color defaultColor;
+        [SerializeField]
+        private Color highlightedColor;
+        [SerializeField]
+        private float animationDuration;
+        [SerializeField]
+        private float selectedScale;
+        [SerializeField]
+        private Vector3 selectedOffset;
+        [SerializeField]
+        private AnimationCurve animationCurve;
+
+        public Color DefaultColor => defaultColor;
+        public Color HighlightedColor => highlightedColor;
+        public float AnimationDuration => animationDuration;
+        public float SelectedScale => selectedScale;
+        public Vector3 SelectedOffset => selectedOffset;
+        public AnimationCurve AnimationCurve => animationCurve;
+    }
+}
--- Assets/Code/ButtonUi.cs
+++ Assets/Code/ButtonUi.cs
@@ -1,5 +1,6 @@
 using TheDungeon.Utils;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -8,15 +9,15 @@ namespace Code
     public class ButtonUi : Selectable
     {
         [SerializeField]
-        private float animationDuration;
-        [SerializeField]
-        private float selectedScale;
-        [SerializeField]
-        private Vector3 selectedOffset;
-        [SerializeField]
-        private AnimationCurve animationCurve;
+        private ButtonConfig buttonConfig;
         [SerializeField]
         private Transform animationTransform;
+        [SerializeField]
+        private Image backgroundImage;
+        [SerializeField]
+        private Text text;
+        [SerializeField]
+        private UnityEvent onClick;
         
         private SimpleTimer _animationTimer;
         
@@ -37,32 +38,53 @@ namespace Code
             PlayExitAnimation();
         }
         
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            base.OnPointerUp(eventData);
+            OnClick();
+        }
+        
         private void PlayEnterAnimation()
         {
-            PlayAnimation(selectedScale, selectedOffset);
+            PlayAnimation(buttonConfig.SelectedScale, buttonConfig.SelectedOffset, buttonConfig.HighlightedColor);
         }
         
         private void PlayExitAnimation()
         {
-            PlayAnimation(1f, Vector3.zero);
+            PlayAnimation(1f, Vector3.zero, buttonConfig.DefaultColor);
         }
         
-        private void PlayAnimation(float endScale, Vector3 endPos)
+        private void OnClick()
+        {
+            onClick.Invoke();
+        }
+        
+        private void PlayAnimation(float endScale, Vector3 endPos, Color endColor)
         {
             var startScale = animationTransform.localScale.x;
             var startPos = animationTransform.localPosition;
+            var startColor = backgroundImage.color;
             _animationTimer = new SimpleTimer();
             _animationTimer.OnUpdate += UpdateAnimation;
-            _animationTimer.Start(animationDuration);
+            _animationTimer.Start(buttonConfig.AnimationDuration);
 
             void UpdateAnimation(float normalizedTime)
             {
                 var vectorStartScale = Vector3.one * startScale;
                 var vectorEndScale = Vector3.one * endScale;
-                var eval = animationCurve.Evaluate(normalizedTime);
+                var eval = buttonConfig.AnimationCurve.Evaluate(normalizedTime);
                 animationTransform.localScale = Vector3.Lerp(vectorStartScale, vectorEndScale, eval);
                 animationTransform.localPosition = Vector3.Lerp(startPos, endPos, eval);
+                backgroundImage.color = Color.Lerp(startColor, endColor, eval);
+                text.color = Color.Lerp(startColor, endColor, eval);
             }
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            backgroundImage.color = buttonConfig.DefaultColor;
+            text.color = buttonConfig.DefaultColor;
         }
     }
     
