3a7fd0695ab0d931e15aab04fe853bfee6c80dd1
jan-rojc <jan.rojc2000@gmail.com>
2025-02-16 19:31:17 +0100

    Added new input actions: jump, dash, use item, drop item

--- Assets/Code/PlayerInputHandler.cs
+++ Assets/Code/PlayerInputHandler.cs
@@ -14,20 +14,73 @@ public class PlayerInputHandler : MonoBehaviour
         _playerInput = GetComponent<PlayerInput>();
     }
 
+    // WASD or Left Stick
     public void OnMove(InputAction.CallbackContext context)
     {
+        // context.started and context.cancelled are called on the first and last frame of performing an action (this function is called twice on those frames)
+        // if (!context.performed) return;
         Debug.Log("Move: " + context.ReadValue<Vector2>());
+        Debug.Log(context);
+        
         // handle movement based on _playerInput.playerIndex
     }
     
+    // Arrows or Right Stick
     public void OnLook(InputAction.CallbackContext context)
     {
         Debug.Log("Look: " + context.ReadValue<Vector2>());
     }
     
-    public void OnFire(InputAction.CallbackContext context)
+    // Q or North Button
+    public void OnDropItem(InputAction.CallbackContext context)
     {
-        Debug.Log("Fire: " +  context);
+        if (context.started)
+        {
+            Debug.Log("Drop Item started");
+        }
+        if (context.canceled)
+        {
+            Debug.Log("Drop Item ended");
+        }
+    }
+    
+    // E or West Button
+    public void OnUseItem(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Use Item started");
+        }
+        if (context.canceled)
+        {
+            Debug.Log("Use Item ended");
+        }
+    }
+    
+    // Ctrl or East Button
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Dash started");
+        }
+        if (context.canceled)
+        {
+            Debug.Log("Dash ended");
+        }
+    }
+    
+    // Space or South Button
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Jump started");
+        }
+        if (context.canceled)
+        {
+            Debug.Log("Jump ended");
+        }
     }
 
     public void OnDeviceLost(PlayerInput playerInput)
