24f9dcf876809197352bd4eb48fd498f876c0b1a
jan-rojc <jan.rojc2000@gmail.com>
2025-04-06 20:25:28 +0200

    Render sorting order implementation

---
+++ Assets/Code/Editor/RendererSortingOrderingEditor.cs
@@ -0,0 +1,58 @@
+﻿using Code.Gameplay;
+using UnityEditor;
+using UnityEngine;
+
+namespace Code.Editor
+{
+    [CustomEditor(typeof(RendererSortingOrderingComponent))]
+    public class RendererSortingOrderingEditor : UnityEditor.Editor
+    {
+
+        private RendererSortingOrderingComponent _targetComponent;
+        private int _allChangeValue;
+    
+        public override void OnInspectorGUI()
+        {
+            // Draw default fields
+            DrawDefaultInspector();
+
+            if (_targetComponent == null)
+            {
+                _targetComponent = (RendererSortingOrderingComponent)target;
+            }
+        
+            GUILayout.Space(10f);
+            GUILayout.Label("Update Sorting Values");
+            GUILayout.Space(5f);
+            foreach (var spriteRenderer in _targetComponent.SpriteRenderers)
+            {
+                spriteRenderer.sortingOrder = EditorGUILayout.IntField($"{spriteRenderer.name} renderer", spriteRenderer.sortingOrder);
+            }
+
+            foreach (var trailRenderer in _targetComponent.TrailRenderers)
+            {
+                trailRenderer.sortingOrder = EditorGUILayout.IntField($"{trailRenderer.name} renderer", trailRenderer.sortingOrder);
+            }
+
+            foreach (var particleSystemRenderers in _targetComponent.ParticleSystemRenderers)
+            {
+                particleSystemRenderers.sortingOrder = EditorGUILayout.IntField($"{particleSystemRenderers.name} renderer", particleSystemRenderers.sortingOrder);
+            }
+        
+            DrawUpdateAllRenderersGUI();
+        }
+
+        private void DrawUpdateAllRenderersGUI()
+        {
+            GUILayout.BeginHorizontal();
+            var newVal = EditorGUILayout.IntField("Change All", _allChangeValue);
+            _allChangeValue = newVal;
+            if (GUILayout.Button("Add To Sorting Order"))
+            {
+                _targetComponent.ChangeOrderInLayer(newVal);
+                EditorUtility.SetDirty(target);
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/Code/Gameplay/RendererSortingOrderingComponent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Code.Gameplay
+{
+    public class RendererSortingOrderingComponent : MonoBehaviour
+    {
+
+        [SerializeField]
+        [HideInInspector]
+        private List<SpriteRenderer> _spriteRenderers = new List<SpriteRenderer>();
+
+        [SerializeField]
+        [HideInInspector]
+        private List<TrailRenderer> _trailRenderers = new List<TrailRenderer>();
+
+        [SerializeField]
+        [HideInInspector]
+        private List<ParticleSystemRenderer> _particleSystemRenderers = new List<ParticleSystemRenderer>();
+
+        public List<SpriteRenderer> SpriteRenderers => _spriteRenderers;
+    
+        public List<TrailRenderer> TrailRenderers => _trailRenderers;
+    
+        public List<ParticleSystemRenderer> ParticleSystemRenderers => _particleSystemRenderers;
+    
+        private void Reset()
+        {
+            _spriteRenderers = GetComponentsInChildren<SpriteRenderer>().ToList();
+            _trailRenderers = GetComponentsInChildren<TrailRenderer>().ToList();
+            _particleSystemRenderers = GetComponentsInChildren<ParticleSystemRenderer>().ToList();
+        }
+
+        public void ChangeOrderInLayer(int amount)
+        {
+            _spriteRenderers.ForEach(x => x.sortingOrder += amount);
+            _trailRenderers.ForEach(x => x.sortingOrder += amount);
+            _particleSystemRenderers.ForEach(x => x.sortingOrder += amount);
+        }
+
+        public void ChangeOrderInLayer(Renderer givenRenderer, int amount)
+        {
+            if (givenRenderer == null)
+            {
+                ChangeOrderInLayer(amount);
+                return;
+            }
+        
+            givenRenderer.sortingOrder += amount;
+        }
+    
+    }
+}
